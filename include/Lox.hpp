a1ef9f66254d354cee01e18c35cfb757b1a89e68
Mehtab Singh <mehtabsinghxx@gmail.com>
2024-07-18 16:09:05 +0100

    chore: Refactor Lox.hpp to separate runFile and runPrompt functions

--- include/Lox.hpp
+++ include/Lox.hpp
@@ -6,17 +6,18 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include "Token.hpp"
+#include "Scanner.hpp"
 
 
 class Lox {
     static bool hadError;
 public:
-    static void run(const std::string& source);
     static void error(int line, const std::string& message);
-    
-private:
     static void runFile(const std::string& path);
     static void runPrompt();
+    
+private:
     static void run(const std::string& source);
     static void report(int line, const std::string& where, const std::string& message);
     
