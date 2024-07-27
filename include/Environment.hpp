94bcffd99b3b2a82934234035f88aaa59d877b83
Mehtab Singh <mehtabsinghxx@gmail.com>
2024-07-27 15:06:04 +0100

    feat: Added new environment for scopes

--- include/Environment.hpp
+++ include/Environment.hpp
@@ -11,6 +11,9 @@
 class Environment {
     std::unordered_map<std::string, std::pair<std::shared_ptr<void>, TokenType>> values;
 public:
+    std::shared_ptr<Environment> enclosing;
+    Environment() : enclosing(nullptr) {}
+    Environment(std::shared_ptr<Environment> enclosing) : enclosing(enclosing) {}
     void define(const std::string& name, const std::pair<std::shared_ptr<void>, TokenType>& value);
     std::pair<std::shared_ptr<void>, TokenType> get(const Token& name);
     std::pair<std::shared_ptr<void>, TokenType> get(const std::string& name);
--- src/Environment.cpp
+++ src/Environment.cpp
@@ -9,20 +9,22 @@ std::pair<std::shared_ptr<void>, TokenType> Environment::get(const Token& name) 
         return values[name.getLexeme()];
     }
 
+    if (enclosing != nullptr) {
+        return (*enclosing).get(name);
+    }
+
     throw std::runtime_error("Undefined variable '" + name.getLexeme() + "'.");
 }
 
-std::pair<std::shared_ptr<void>, TokenType> Environment::get(const std::string& name) {
-    if (values.find(name) != values.end()) {
-        return values[name];
-    }
-
-    throw std::runtime_error("Undefined variable '" + name + "'.");
-}
 
 void Environment::assign(const Token& name, const std::pair<std::shared_ptr<void>, TokenType>& value) {
     if (values.find(name.getLexeme()) != values.end()) {
         values[name.getLexeme()] = value;
+        return;
+    }
+
+    if (enclosing != nullptr) {
+        (*enclosing).assign(name, value);
         return;
     }
 
