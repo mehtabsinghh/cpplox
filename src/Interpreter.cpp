92e718eab651b44ded308bb3d4fd915af658da45
Mehtab Singh <mehtabsinghxx@gmail.com>
2024-09-02 10:32:03 +0100

    Handled string concatenation

--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -7,156 +7,29 @@
 #include "ReturnException.hpp"
 
 Interpreter::Interpreter() {
-    globals->define("clock", std::make_pair(std::make_shared<Clock>(), TokenType::FUN));
+    globals->define("clock", std::make_pair(std::make_shared<Clock>(), TokenType::FUN)); // Add the clock function to the global environment
 }
 
-void Interpreter::visitLiteral(const Literal& expr) {
-    result = expr.value;
-    type = expr.type;
-}
-
-void Interpreter::visitLogical(const Logical& expr) {
-    std::shared_ptr<void> left = evaluate(*expr.left);
-    TokenType leftType = getType();
-
-    if (expr.op.getType() == TokenType::OR) {
-        if (isTruthy(left, leftType)) {
-            result = left;
-            type = leftType;
-            return;
-        }
-    } else {
-        if (!isTruthy(left, leftType)) {
-            result = left;
-            type = leftType;
-            return;
-        }
-    }
-
-    result = evaluate(*expr.right);
-    type = getType();
-}
-
-void Interpreter::visitGrouping(const Grouping& expr) {
-    result = evaluate(*expr.expression);
-}
-
-std::shared_ptr<void> Interpreter::evaluate(const Expr& expr) {
-    expr.accept(*this);
-    return result;
-}
-
-void Interpreter::visitExpression(const Expression& stmt) {
-    evaluate(*stmt.expression);
-}
-
-void Interpreter::visitFunction(const Function& stmt) {
-    std::shared_ptr<LoxFunction> function = std::make_shared<LoxFunction>(std::make_unique<Function>(stmt), environment);
-    environment->define(stmt.name.getLexeme(), std::make_pair(function, TokenType::FUN));
-}
-
-void Interpreter::visitIf(const If& stmt) {
-    if (isTruthy(evaluate(*stmt.condition), getType())) {
-        execute(*stmt.thenBranch);
-    } else if (stmt.elseBranch != nullptr) {
-        execute(*stmt.elseBranch);
-    }
-}
-
-void Interpreter::visitPrint(const Print& stmt) {
-    std::shared_ptr<void> value = evaluate(*stmt.expression);
-    std::cout << stringify(value, type) << std::endl;
-}
-
-void Interpreter::visitReturn(const Return& stmt) {
-    std::shared_ptr<void> value = nullptr;
-    if (stmt.value != nullptr) {
-        value = evaluate(*stmt.value);
-    }
-    throw ReturnException(value, type);
-}
-
-void Interpreter::visitVar(const Var& stmt) {
-    std::shared_ptr<void> value = nullptr;
-    if (stmt.initializer != nullptr) {
-        value = evaluate(*stmt.initializer);
-    }
-    
-    environment->define(stmt.name.getLexeme(), std::make_pair(value, type));
-}
-
-void Interpreter::visitWhile(const While& stmt) {
-    while (isTruthy(evaluate(*stmt.condition), type)) {
-        execute(*stmt.body);
-    }
-}
-
-void Interpreter::visitAssign(const Assign& stmt) {
-    std::shared_ptr<void> value = evaluate(*stmt.value);
-    TokenType valueType = getType();
-    environment->assign(stmt.name, std::make_pair(value, valueType));
-}
-
-void Interpreter::visitVariable(const Variable& expr) {
-    std::pair<std::shared_ptr<void>, TokenType> value = environment->get(expr.name);
-    result = value.first;
-    type = value.second;
-}
-
-void Interpreter::execute(const Stmt& stmt) {
-    stmt.accept(*this);
-}
-
-void Interpreter::visitBlock(const Block& stmt) {
-    executeBlock(stmt.statements, std::make_shared<Environment>(environment));
-}
-
-void Interpreter::executeBlock(std::vector<std::shared_ptr<Stmt>> statements, std::shared_ptr<Environment> environment) {
-    std::shared_ptr<Environment> previous = this->environment;
+void Interpreter::interpret(const std::vector<std::shared_ptr<Stmt>>& statements) {
     try {
-        this->environment = environment;
+        // Execute each statement
         for (const auto& statement : statements) {
             execute(*statement);
         }
     } catch (const RuntimeError& error) {
+        // Catch any runtime errors and print them
         Lox::runtimeError(error);
     }
-    this->environment = previous;
-}
-
-void Interpreter::visitUnary(const Unary& expr) {
-    std::shared_ptr<void> right = evaluate(*expr.right);
-    TokenType rightType = getType();
-    switch (expr.op.getType()) {
-        case TokenType::MINUS:
-            checkNumberOperand(expr.op, rightType);
-            result = std::make_shared<double>(-*std::static_pointer_cast<double>(right));
-            break;
-        case TokenType::BANG:
-            result = std::make_shared<bool>(!isTruthy(right, rightType));
-            type = !isTruthy(right, rightType) ? TokenType::TRUE : TokenType::FALSE;
-            break;
-        default:
-            break;
-    }
-}
-
-bool Interpreter::isTruthy(const std::shared_ptr<void>& object, TokenType type) {
-    if (type == TokenType::FALSE) {
-        return false;
-    } else if (type == TokenType::NIL) {
-        return false;
-    } 
-    return true;
 }
 
 void Interpreter::visitBinary(const Binary& expr) {
+    // Evaluate the left and right expressions
     std::shared_ptr<void> left = evaluate(*expr.left);
     const TokenType leftType = getType();
     std::shared_ptr<void> right = evaluate(*expr.right);
     const TokenType rightType = getType();
-    
 
+    // Perform the operation based on the operator type
     switch (expr.op.getType()) {
         case TokenType::GREATER:
             checkNumberOperands(expr.op, leftType, rightType);
@@ -192,7 +65,6 @@ void Interpreter::visitBinary(const Binary& expr) {
             type = TokenType::NUMBER;
             break;
         case TokenType::PLUS:
-            checkNumberOperands(expr.op, leftType, rightType);
             if (leftType == TokenType::NUMBER && rightType == TokenType::NUMBER) {
                 // If both are numbers, add them
                 result = std::make_shared<double>(*std::static_pointer_cast<double>(left) + *std::static_pointer_cast<double>(right));
@@ -201,6 +73,9 @@ void Interpreter::visitBinary(const Binary& expr) {
                 // If both are strings, concatenate them
                 result = std::make_shared<std::string>(*std::static_pointer_cast<std::string>(left) + *std::static_pointer_cast<std::string>(right));
                 type = TokenType::STRING;
+            } else {
+                // Otherwise, throw an error
+                throw RuntimeError(expr.op, "Operands must be two numbers or two strings.");
             }
             break;
         case TokenType::SLASH:
@@ -218,32 +93,58 @@ void Interpreter::visitBinary(const Binary& expr) {
     }
 }
 
-void outputType(TokenType type) {
-    switch (type) {
-        case TokenType::NUMBER:
-            std::cout << "NUMBER" << std::endl;
+void Interpreter::visitGrouping(const Grouping& expr) {
+    result = evaluate(*expr.expression);
+}
+
+void Interpreter::visitLiteral(const Literal& expr) {
+    result = expr.value;
+    type = expr.type;
+}
+
+void Interpreter::visitUnary(const Unary& expr) {
+    std::shared_ptr<void> right = evaluate(*expr.right);
+    TokenType rightType = getType();
+    switch (expr.op.getType()) {
+        case TokenType::MINUS:
+            checkNumberOperand(expr.op, rightType);
+            result = std::make_shared<double>(-*std::static_pointer_cast<double>(right));
             break;
-        case TokenType::STRING:
-            std::cout << "STRING" << std::endl;
-            break;
-        case TokenType::TRUE:
-            std::cout << "TRUE" << std::endl;
-            break;
-        case TokenType::FALSE:
-            std::cout << "FALSE" << std::endl;
-            break;
-        case TokenType::NIL:
-            std::cout << "NIL" << std::endl;
-            break;
-        case TokenType::FUN:
-            std::cout << "FUN" << std::endl;
-            break;
-        case TokenType::CLASS:
-            std::cout << "CLASS" << std::endl;
+        case TokenType::BANG:
+            result = std::make_shared<bool>(!isTruthy(right, rightType));
+            type = !isTruthy(right, rightType) ? TokenType::TRUE : TokenType::FALSE;
             break;
         default:
             break;
     }
+}
+
+void Interpreter::visitVariable(const Variable& expr) {
+    std::pair<std::shared_ptr<void>, TokenType> value = environment->get(expr.name);
+    result = value.first;
+    type = value.second;
+}
+
+void Interpreter::visitLogical(const Logical& expr) {
+    std::shared_ptr<void> left = evaluate(*expr.left);
+    TokenType leftType = getType();
+
+    if (expr.op.getType() == TokenType::OR) {
+        if (isTruthy(left, leftType)) {
+            result = left;
+            type = leftType;
+            return;
+        }
+    } else {
+        if (!isTruthy(left, leftType)) {
+            result = left;
+            type = leftType;
+            return;
+        }
+    }
+
+    result = evaluate(*expr.right);
+    type = getType();
 }
 
 void Interpreter::visitCall(const Call& expr) {
@@ -269,24 +170,114 @@ void Interpreter::visitCall(const Call& expr) {
     type = returnType;
 }
 
-bool Interpreter::isEqual(const std::shared_ptr<void>& a, const std::shared_ptr<void>& b, TokenType aType, TokenType bType) {
-    if (aType != bType) 
+void Interpreter::visitExpression(const Expression& stmt) {
+    evaluate(*stmt.expression);
+}
+
+void Interpreter::visitFunction(const Function& stmt) {
+    std::shared_ptr<LoxFunction> function = std::make_shared<LoxFunction>(std::make_unique<Function>(stmt), environment);
+    environment->define(stmt.name.getLexeme(), std::make_pair(function, TokenType::FUN));
+}
+
+void Interpreter::visitPrint(const Print& stmt) {
+    std::shared_ptr<void> value = evaluate(*stmt.expression);
+    std::cout << stringify(value, type) << std::endl;
+}
+
+void Interpreter::visitVar(const Var& stmt) {
+    std::shared_ptr<void> value = nullptr;
+    if (stmt.initializer != nullptr) {
+        value = evaluate(*stmt.initializer);
+    }
+    
+    environment->define(stmt.name.getLexeme(), std::make_pair(value, type));
+}
+
+void Interpreter::visitAssign(const Assign& stmt) {
+    std::shared_ptr<void> value = evaluate(*stmt.value);
+    TokenType valueType = getType();
+    environment->assign(stmt.name, std::make_pair(value, valueType));
+}
+
+std::shared_ptr<void> Interpreter::evaluate(const Expr& expr) {
+    expr.accept(*this);
+    return result;
+}
+
+void Interpreter::visitBlock(const Block& stmt) {
+    executeBlock(stmt.statements, std::make_shared<Environment>(environment));
+}
+
+void Interpreter::visitIf(const If& stmt) {
+    if (isTruthy(evaluate(*stmt.condition), getType())) {
+        execute(*stmt.thenBranch);
+    } else if (stmt.elseBranch != nullptr) {
+        execute(*stmt.elseBranch);
+    }
+}
+
+void Interpreter::visitWhile(const While& stmt) {
+    while (isTruthy(evaluate(*stmt.condition), type)) {
+        execute(*stmt.body);
+    }
+}
+
+void Interpreter::visitReturn(const Return& stmt) {
+    std::shared_ptr<void> value = nullptr;
+    if (stmt.value != nullptr) {
+        value = evaluate(*stmt.value);
+    }
+    throw ReturnException(value, type);
+}
+
+void Interpreter::executeBlock(std::vector<std::shared_ptr<Stmt>> statements, std::shared_ptr<Environment> environment) {
+    std::shared_ptr<Environment> previous = this->environment;
+    try {
+        this->environment = environment;
+        for (const auto& statement : statements) {
+            execute(*statement);
+        }
+    } catch (const RuntimeError& error) {
+        Lox::runtimeError(error);
+    }
+    this->environment = previous;
+}
+
+std::shared_ptr<void>& Interpreter::getResult() {
+    return result;
+}
+
+TokenType Interpreter::getType() {
+    return type;
+}
+
+bool Interpreter::isTruthy(const std::shared_ptr<void>& object, TokenType type) {
+    if (type == TokenType::FALSE) {
         return false;
-    if (aType == TokenType::TRUE || aType == TokenType::FALSE) 
-        return aType == bType;
-    if (a == nullptr && b == nullptr) {
+    } else if (type == TokenType::NIL) {
+        return false;
+    } 
+    return true;
+}
+
+bool Interpreter::isEqual(const std::shared_ptr<void>& left, const std::shared_ptr<void>& right, TokenType leftType, TokenType rightType) {
+    if (leftType != rightType) 
+        return false;
+    if (leftType == TokenType::TRUE || leftType == TokenType::FALSE) 
+        return leftType == rightType;
+    if (left == nullptr && right == nullptr) {
         std::cout << "Both are null" << std::endl;
         return true;
     }
-    if (a == nullptr) 
+    if (left == nullptr) 
         return false;
     
-    if (aType == TokenType::NUMBER) 
-        return *std::static_pointer_cast<double>(a) == *std::static_pointer_cast<double>(b);
-    if (aType == TokenType::STRING) 
-        return *std::static_pointer_cast<std::string>(a) == *std::static_pointer_cast<std::string>(b);
-    if (aType == TokenType::TRUE || aType == TokenType::FALSE) 
-        return *std::static_pointer_cast<bool>(a) == *std::static_pointer_cast<bool>(b);
+    if (leftType == TokenType::NUMBER) 
+        return *std::static_pointer_cast<double>(left) == *std::static_pointer_cast<double>(right);
+    if (leftType == TokenType::STRING) 
+        return *std::static_pointer_cast<std::string>(left) == *std::static_pointer_cast<std::string>(right);
+    if (leftType == TokenType::TRUE || leftType == TokenType::FALSE) 
+        return *std::static_pointer_cast<bool>(left) == *std::static_pointer_cast<bool>(right);
     
 
     return false;
@@ -304,16 +295,6 @@ void Interpreter::checkNumberOperands(const Token op, const TokenType leftType, 
         return;
     }
     throw RuntimeError(op, "Operands must be numbers.");
-}
-
-void Interpreter::interpret(const std::vector<std::shared_ptr<Stmt>>& statements) {
-    try {
-        for (const auto& statement : statements) {
-            execute(*statement);
-        }
-    } catch (const RuntimeError& error) {
-        Lox::runtimeError(error);
-    }
 }
 
 std::string Interpreter::stringify(const std::shared_ptr<void>& object, TokenType type) {
@@ -344,10 +325,8 @@ std::string Interpreter::stringify(const std::shared_ptr<void>& object, TokenTyp
     return "nil";
 }
 
-std::shared_ptr<void>& Interpreter::getResult() {
-    return result;
+void Interpreter::execute(const Stmt& stmt) {
+    stmt.accept(*this);
 }
 
-TokenType Interpreter::getType() {
-    return type;
-}
+
